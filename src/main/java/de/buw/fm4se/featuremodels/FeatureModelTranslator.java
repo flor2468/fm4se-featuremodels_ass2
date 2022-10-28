3eb56f126bf8cc8846f0b541269086131591e89a
flor2468 <jenny.doering@uni-weimar.de>
2022-10-28 12:55:52 +0200

    started task 1

--- src/main/java/de/buw/fm4se/featuremodels/FeatureModelTranslator.java
+++ src/main/java/de/buw/fm4se/featuremodels/FeatureModelTranslator.java
@@ -1,12 +1,28 @@
 package de.buw.fm4se.featuremodels;
 
-import de.buw.fm4se.featuremodels.fm.FeatureModel;
+import java.util.List;
+
+//import de.buw.fm4se.featuremodels.fm.FeatureModel;
+import de.buw.fm4se.featuremodels.fm.*;
 
 public class FeatureModelTranslator {
   public static String translateToFormula(FeatureModel fm) {
     
     // TODO implement a real translation
-    
-    return "a & !a";
+	  
+	List<Feature> allChildren = fm.getRoot().getChildren();
+	String result = fm.getRoot().getName();
+	
+	for (int i = 0; i < allChildren.size(); i++) {
+		result += " & (" + allChildren.get(i).getName() + " -> " + fm.getRoot().getName() + ")";
+		
+		if (allChildren.get(i).isMandatory()) {
+			result += " & (" + fm.getRoot().getName() + " -> " + allChildren.get(i).getName() + ")";
+		}
+	}
+	
+	System.out.println("TEST: " + result);
+	    
+	return result;
   }
 }
